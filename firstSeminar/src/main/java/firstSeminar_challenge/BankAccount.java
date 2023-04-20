7fc0cd862597af72612d8b07b1728ebdfa2e2a8c
sunny <rosa7536@naver.com>
2023-04-20 15:26:04 +0900

    Create: 1st-seminar-challenge

---
+++ firstSeminar/src/main/java/firstSeminar_challenge/BankAccount.java
@@ -0,0 +1,8 @@
+package firstSeminar_challenge;
+
+public interface BankAccount {
+    abstract public void deposit(int money);
+    abstract public void withdraw(int money);
+    abstract public void getBalance();
+
+}
